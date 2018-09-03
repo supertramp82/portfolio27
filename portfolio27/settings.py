ed719bdf81f32228899761275ca275554ce62fe9
Oleg K <olegk65@gmail.com>
2018-09-03 16:06:06 -0700

    admin site css

--- portfolio27/settings.py
+++ portfolio27/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio27/static')
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
