4320ccfaf591b8e13661f373431da9aabf8a16c1
Oleg K <olegk65@gmail.com>
2018-09-03 16:02:42 -0700

    admin site css

--- portfolio27/settings.py
+++ portfolio27/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
